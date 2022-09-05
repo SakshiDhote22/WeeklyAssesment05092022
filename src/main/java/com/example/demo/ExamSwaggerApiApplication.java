8a3783ee9316567f0024c73cf21f956571fca8fb
SakshiDhote22 <110605988+SakshiDhote22@users.noreply.github.com>
2022-09-05 09:48:04 +0530

    Update ExamSwaggerApiApplication.java

--- src/main/java/com/example/demo/ExamSwaggerApiApplication.java
+++ src/main/java/com/example/demo/ExamSwaggerApiApplication.java
@@ -1,3 +1,6 @@
+/*2)write a program to describe Swagger API documentation.
+  (hint: try to use GET,POST,PUT,DELTE)*/
+
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
