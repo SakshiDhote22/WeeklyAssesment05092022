88a2535529c5add822194cc3f98e26a99da83721
SakshiDhote22 <110605988+SakshiDhote22@users.noreply.github.com>
2022-09-05 10:19:36 +0530

    Add files via upload

---
+++ FirstCrudGradle/ExamCrudGradleApplication.java
@@ -0,0 +1,15 @@
+/*1)Develop an application using SpringBoot with RestFull Web Services(CRUD Operations) using Gradle configuration.*/
+
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamCrudGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamCrudGradleApplication.class, args);
+	}
+
+}
