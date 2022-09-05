2387fbcfb6fada1fe5467d25b9af92206f0e5f41
SakshiDhote22 <110605988+SakshiDhote22@users.noreply.github.com>
2022-09-05 09:46:13 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/TeacherService.java
@@ -0,0 +1,50 @@
+package com.example.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.repository.TeacherRepo;
+import com.example.demo.model.*;
+
+@Service
+
+public class TeacherService {
+
+ @Autowired
+
+ TeacherRepo teacherRepository;
+
+ public String saveEmployee(Teacher teacher) {
+
+	 teacherRepository.save(teacher);
+
+ return "Teacher Information saved Successfully";
+
+ }
+
+ public List<Teacher> getTeacher(){
+
+ return teacherRepository.findAll();
+
+ }
+
+ public Teacher updateTeacher(int id) {
+
+ return teacherRepository.getOne(id);
+
+ }
+
+ public void deleteTeacher(int id) {
+
+	 teacherRepository.deleteById(id);
+
+ }
+
+}
+
+
+
+
