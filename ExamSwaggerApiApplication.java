cbdd7779d6f4851b2f266f34abe28728d5cb7ec0
SakshiDhote22 <110605988+SakshiDhote22@users.noreply.github.com>
2022-09-05 09:41:39 +0530

    Add files via upload

---
+++ ExamSwaggerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamSwaggerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamSwaggerApiApplication.class, args);
+	}
+
+}
